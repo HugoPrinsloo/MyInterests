0f31bab3024ccbbc3f2aa9f6a8f67c7566583816
Hugo Prinsloo <hugo.prinsloo@icloud.com>
2018-08-09 13:43:17 +0200

    Basic UI done

---
+++ My Interests/My Interests/NewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewViewController.swift
+//  My Interests
+//
+//  Created by Hugo Prinsloo on 2018/08/09.
+//  Copyright © 2018 Hugo. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
