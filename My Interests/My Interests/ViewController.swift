a1dbd4dcf58cae417408dea23915cfd370e2d863
Hugo Prinsloo <hugo.prinsloo@icloud.com>
2018-08-09 12:20:40 +0200

    Created xcode project

---
+++ My Interests/My Interests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Interests
+//
+//  Created by Hugo Prinsloo on 2018/08/09.
+//  Copyright © 2018 Hugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
